b0c76692c2085252ef9132870f378e286a01c5d8
Avi711 <avraham711@gmail.com>
2023-04-27 17:43:28 +0300

    moving robot with simple obstacle avoidance

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <libplayerc++/playerc++.h>
+
+using namespace std;
+using namespace PlayerCc;
+
+
+int main(int argc, char *argv[]) {
+
+    PlayerClient robot("localhost", 6665);
+    Position2dProxy  pos2d(&robot,0);
+    RangerProxy      sonarProxy(&robot,0);
+
+    // enable motors
+    pos2d.SetMotorEnable(1);
+
+    // request geometries
+    pos2d.RequestGeom();
+    sonarProxy.RequestGeom();
+    pos2d.SetSpeed(10,0);
+
+    while (true) {
+        // read from the proxies
+        robot.Read();
+        cout << sonarProxy[0] << endl;
+        if (sonarProxy[0] < 1.5) {
+            pos2d.SetSpeed(0,-1 * 90);
+        }
+        else {
+            pos2d.SetSpeed(10,0);
+        }
+
+
+    }
+    return 0;
+}
