242d3d4c45ab3ffa30d7e9d533c74cee8f08580b
Kehat Sudri <keatsudri@gmail.com>
2023-05-12 18:07:44 +0300

    add few points

--- src/graph.cpp
+++ src/graph.cpp
@@ -6,7 +6,11 @@ double Vertex::getY() {return this->y;}
 
 // TODO implement this func for real with dijkstra or something else
 std::vector<Vertex> getRoute(Vertex start, Vertex goal){
-    std::vector<Vertex> v = {{1.33,-5.7}, {1.3,3},{1.33,-5.7}, {-7.22, -5.70} };
+    std::vector<Vertex> v = {{1.33,-5.7}, {1.3,3},{1.33,-5.7}, {-7.22, -5.70},
+                             {-8.70 -9.65},{-12.48,-9.70},{-12.48,-4.28},{-12.48,0.29},{-12.48,3.16},{-12.48,6.07},{-12.48,7.17}
+                             ,{-12.48 11.78},{-12.48 14.77},{-12.48,15.99},{-12.48,18.90},{-6.45,19.57},{-3.58,19.57}};
+
+
     return v;
 }
 
